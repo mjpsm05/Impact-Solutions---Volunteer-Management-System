1fabe09a8ef7eda1f2e1a16237c5d5d2914868ea
ChristaRobinson <98274541+ChristaRobinson@users.noreply.github.com>
2026-07-30 12:57:07 -0400

    Init
    JetBrains Rider .Net 8 M-V-C project created

---
+++ volunteer-management/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace volunteer_management.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
